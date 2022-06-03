1eabdf48886c3927c50dc17c840b205c86f50b31
jungho shin <wjdghtls11@gmail.com>
2022-06-03 19:14:15 +0900

    Add: mongodb 연결

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
+require('dotenv').config();
+const { PORT, MONGODB_URI } = process.env;
+
 const express = require('express');
+const MongoClient = require('mongodb/lib/mongo_client');
 const app = express();
+
+// mongodb 접속 후에 8080서버 열린다는 뜻
+var db;
+MongoClient.connect(MONGODB_URI, { useUnifiedTopology: true },function(err, client) {
+  // 에러처리
+  if(err) return console.log('err')
+  
+  db = client.db('todoApp'); 
+
+  db.collection('post').insertOne( { 이름: 'John', _id: 1, 나이: 20 }, function(err, result) {
+    console.log('save complete');
+  });
+
+  app.listen(PORT, function () {
+    console.log(`running on ${PORT}...`);
+  });
+})
 
 app.use(express.urlencoded({extended: true})) // body parser express에 다 저장되어있어서 따로 설치필요x
 
-app.listen(8080, function () {
-  console.log('running on 8080...');
-});
-
-// app.get('/pet', function (req, res) {
-//   res.send('펫용품 쇼핑할 수 있는 페이지 입니다.');
-// })
-
-// app.get('/beauty', function (req, res) {
-//   res.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.');
-// })
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -24,8 +34,8 @@ app.get('/write', function (req, res) {
 })
 
 app.post('/add', function (req, res) { // req에 post 보낸거 저장
-  res.send('전송완료')
-  console.log(req.body.date);
-  console.log(req.body.title);
-  console.log(req.body);
+  db.collection('post').insertOne({ 제목 : req.body.title, 날짜 : req.body.date }, function (err, result) {
+    console.log('save complete');
+  })
+  console.log('전송완료');
 })
