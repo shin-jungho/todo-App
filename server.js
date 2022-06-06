fc7da980906e76407fae6a965339b69c352e6003
jungho shin <wjdghtls11@gmail.com>
2022-06-06 16:34:31 +0900

    refactor: 환경 변수 설정

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb/lib/mongo_client');
 require('dotenv').config();
 const { PORT, MONGODB_URI } = process.env;
+const port = PORT || 4000;
 
-const express = require('express');
-const MongoClient = require('mongodb/lib/mongo_client');
-const app = express();
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
 
 // mongodb 접속 후에 8080서버 열린다는 뜻
 var db;
 MongoClient.connect(MONGODB_URI, { useUnifiedTopology: true },function(err, client) {
   // 에러처리
-  if(err) return console.log('err')
+  if(err) {return console.log('err')}
   
   db = client.db('todoApp'); 
 
-  db.collection('post').insertOne( { 이름: 'John', _id: 1, 나이: 20 }, function(err, result) {
-    console.log('save complete');
-  });
+  // db.collection('post').insertOne( { 이름: 'John', _id: 1, 나이: 20 }, function(err, result) {
+  //   console.log('save complete');
+  // });
 
-  app.listen(PORT, function () {
-    console.log(`running on ${PORT}...`);
+  app.listen(port, function () {
+    console.log(`running on ${port}...`);
   });
 })
 
@@ -34,8 +37,12 @@ app.get('/write', function (req, res) {
 })
 
 app.post('/add', function (req, res) { // req에 post 보낸거 저장
-  db.collection('post').insertOne({ 제목 : req.body.title, 날짜 : req.body.date }, function (err, result) {
+  console.log('전송완료');
+  db.collection('post').insertOne({ 제목 : req.body.title, 날짜 : req.body.date }, function () {
     console.log('save complete');
   })
-  console.log('전송완료');
+})
+
+app.get('/list', function (req, res) {
+  res.render('list.ejs');
 })
