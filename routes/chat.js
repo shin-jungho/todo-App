38e95f71df67992f3ede4b6e1d1e1517f25297a5
jungho shin <wjdghtls11@gmail.com>
2022-06-16 15:42:58 +0900

    Fix : detail, edit page 에러수정

--- routes/chat.js
+++ routes/chat.js
@@ -1,3 +1,25 @@
 import express from 'express';
 
 const router= express.Router();
+
+app.get('/chat', isLogin, function(req, res){ 
+
+  db.collection('chatroom').find({ member : req.user._id }).toArray().then((result)=>{
+    console.log(result);
+    응답.render('chat.ejs', {data : result})
+  })
+
+}); 
+
+router.post('/chatroom', function(req, res){
+
+  var chatSave = {
+    title : '무슨무슨채팅방',
+    member : [ObjectId(req.body.당한사람id), req.user._id],
+    date : new Date()
+  }
+
+  db.collection('chatroom').insertOne(chatSave).then(function(result){
+    res.send('저장완료')
+  });
+});
--- routes/post.js
+++ routes/post.js
@@ -39,12 +39,28 @@ router.get('/list', async (req, res) => {
 });
 
 router.get('/search', (req, res) => {
-  postCollection.find({ 제목: req.query.value }).toArray((err, result) => {
-    res.render('search.ejs', { 
-      posts : result, 
-      사용자 : req.user ? req.user.id : undefined
-    });
-  });
+  //index만들어둔걸로 빠르게 검색하려면
+  var searchOption = [
+    {
+      $search : {
+        index : 'titleSearch',
+        text : {
+          query : req.query.value,
+          path : 'title'
+        }
+      }
+    },
+    // 검색조건 더 주는 법 : - 결과 정렬하기
+    { $sort : { _id : -1 } },
+    { $limit : 10 }, // 갯수 제한 두기
+    { $project : { title : 1, _id : 0, score : { $meta : "searchScore" } } } // 검색결과에서 필터주기
+  ]
+
+  postCollection.aggregate(searchOption).toArray((err, result) => {
+    console.log(result);
+    res.render('search.ejs', { posts : result });
+  })
+
 });
 
 router.delete('/delete', async (req, res) => {
@@ -67,7 +83,7 @@ router.get('/detail/:id', async (req, res) => {
       사용자: req.user ? req.user.id : undefined,
     });
   } else {
-    res.status(404).render('detail.ejs', {
+    res.status(400).render('detail.ejs', {
       error: 'The post is not found',
       사용자: req.user ? req.user.id : undefined,
     });
